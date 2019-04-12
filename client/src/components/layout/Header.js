e632427fa4834bf8a88da9d9182bcf6fe55972eb
mainman100 <rstantonreid@gmail.com>
2019-04-12 14:05:27 -0400

    navbar services

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap/dist/react-bootstrap";
 import styles from "./Header.module.scss";
 import classnames from "classnames";
+import { showServices } from "./HeaderData";
+
 import { logoutUser } from "../../actions/authActions";
 
 class Header extends Component {
@@ -17,6 +19,8 @@ class Header extends Component {
   render() {
     const { isAuthenticated, user } = this.props.auth;
 
+    const services = showServices;
+
     const authLinks = (
       <Nav className="ml-auto">
         <NavDropdown title="Account" id="basic-nav-dropdown">
@@ -24,11 +28,7 @@ class Header extends Component {
             Your Account
           </NavDropdown.Item>
           <NavDropdown.Item>
-            <a
-              href=""
-              onClick={this.onLogoutClick.bind(this)}
-              
-            >
+            <a href="" onClick={this.onLogoutClick.bind(this)}>
               <img
                 className="rounded-circle"
                 src={user.avatar}
@@ -68,62 +68,118 @@ class Header extends Component {
             <Nav className="mr-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <NavDropdown title="Aesthetics" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/aesthetics">Overview</NavDropdown.Item>
+                <NavDropdown.Item className={classnames(styles.aestheticsOverview)} href="/aesthetics">Overview</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item
-                  className={classnames(styles.aesthetics)}
-                  href="/aesthetics/laserhair"
-                >
-                  Laser Hair Removal
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.aesthetics)}
-                  href="/aesthetics/acnescars"
-                >
-                  Acne Scars
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.aesthetics)}
-                  href="/aesthetics/botox"
-                >
-                  Botox
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.aesthetics)}
-                  href="/aesthetics/ipl"
-                >
-                  IPL
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.aesthetics)}
-                  href="/aesthetics/sculpsure"
-                >
-                  Sculpsure
-                </NavDropdown.Item>
+                {services.laserhair ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/laserhair"
+                  >
+                    Laser Hair Removal
+                  </NavDropdown.Item>
+                ) : null}
+                {services.acnescars ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/acnescars"
+                  >
+                    Acne Scars
+                  </NavDropdown.Item>
+                ) : null}
+                {services.botox ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/botox"
+                  >
+                    Botox
+                  </NavDropdown.Item>
+                ) : null}
+                {services.ipl ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/ipl"
+                  >
+                    IPL
+                  </NavDropdown.Item>
+                ) : null}
+                {services.sculpsure ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/sculpsure"
+                  >
+                    Sculpsure
+                  </NavDropdown.Item>
+                ) : null}
+                {services.juvederm ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/juvederm"
+                  >
+                    Juvederm
+                  </NavDropdown.Item>
+                ) : null}
+                {services.weightloss ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/weightloss"
+                  >
+                    Weight Loss
+                  </NavDropdown.Item>
+                ) : null}
+                {services.skincare ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.aesthetics)}
+                    href="/aesthetics/skincare"
+                  >
+                    Skin Care
+                  </NavDropdown.Item>
+                ) : null}
               </NavDropdown>
               <NavDropdown title="Health Zones" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/healthzones">
+                <NavDropdown.Item className={classnames(styles.healthOverview)} href="/healthzones">
                   Overview
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item
-                  className={classnames(styles.health)}
-                  href="/healthzones/ozone"
-                >
-                  Ozone Sauna
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.health)}
-                  href="/healthzones/infrared"
-                >
-                  Infrared Sauna
-                </NavDropdown.Item>
-                <NavDropdown.Item
-                  className={classnames(styles.health)}
-                  href="/healthzones/salt"
-                >
-                  Salt Therapy
-                </NavDropdown.Item>
+                {services.ozone ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.health)}
+                    href="/healthzones/ozone"
+                  >
+                    Ozone Sauna
+                  </NavDropdown.Item>
+                ) : null}
+                {services.infrared ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.health)}
+                    href="/healthzones/infrared"
+                  >
+                    Infrared Sauna
+                  </NavDropdown.Item>
+                ) : null}
+                {services.salt ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.health)}
+                    href="/healthzones/salt"
+                  >
+                    Salt Therapy
+                  </NavDropdown.Item>
+                ) : null}
+                {services.cryotherapy ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.health)}
+                    href="/healthzones/cryotherapy"
+                  >
+                    Cryotherapy
+                  </NavDropdown.Item>
+                ) : null}
+                {services.dripbar ? (
+                  <NavDropdown.Item
+                    className={classnames(styles.health)}
+                    href="/healthzones/dripbar"
+                  >
+                    Drip Bar
+                  </NavDropdown.Item>
+                ) : null}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
@@ -135,15 +191,15 @@ class Header extends Component {
               Lake Nona, Orlando
             </Navbar.Text>
           </Navbar.Collapse>
-          {isAuthenticated ? (<Navbar.Collapse id="basic-navbar-nav">
-            <Navbar.Text
-              style={{ textTransform: "capitalize " }}
-            >
-             welcome {user.name}
-            </Navbar.Text>
-          </Navbar.Collapse>) : null}
-          
-          <Navbar.Collapse id="basic-navbar-nav">
+          {isAuthenticated ? (
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Navbar.Text style={{ textTransform: "capitalize " }}>
+                welcome {user.name}
+              </Navbar.Text>
+            </Navbar.Collapse>
+          ) : null}
+
+          <Navbar.Collapse className={classnames(styles.accountDropdown)} id="basic-navbar-nav">
             {isAuthenticated ? authLinks : guestLinks}
             {/*  <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
---
+++ client/src/components/layout/HeaderData.js
@@ -0,0 +1,15 @@
+export const showServices = {
+  laserhair: true,
+  botox: true,
+  sculpsure: true,
+  ipl: true,
+  acnescars: true,
+  juvederm: true,
+  weightloss: false,
+  skincare: false,
+  ozone: true,
+  infrared: true,
+  salt: true,
+  dripbar: true,
+  cryotherapy: false
+}
